8b13b5946db12546b19a70b83d704119c8ff7478
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-09-12 16:44:04 +0530

    created the animation

--- src/app/collaboration/page.jsx
+++ src/app/collaboration/page.jsx
@@ -1,12 +1,48 @@
+"use client";
+
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import logo from "../../../public/favicon.ico";
 import Link from "next/link";
+import logo2 from "../../../public/logo.png";
+import { useEffect } from "react";
 
 function page() {
+  useEffect(() => {
+    const logospan = document.querySelectorAll(`.${styles.logo2}`);
+    const intro = document.querySelector(`.${styles.intro}`);
+
+    setTimeout(() => {
+      logospan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add(styles.active);
+        }, (idx + 1) * 400);
+      });
+
+      setTimeout(() => {
+        logospan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.remove(styles.active);
+            span.classList.add(styles.fade);
+          }, (idx + 1) * 50);
+        });
+      }, 2000);
+
+      setTimeout(() => {
+        intro.style.top = "-100vh";
+      }, 2300);
+    }, 0);
+
+    return () => {};
+  }, []);
   return (
     <div className={styles.main}>
+      <div className={styles.intro}>
+        <span className={styles.logo2}>
+          <Image src={logo2} alt="logo" />
+        </span>
+      </div>
       <div className={styles.container}>
         <div className={styles.logo}>
           <Image src={logo} alt="logo" height={180} />
@@ -29,7 +65,12 @@ function page() {
           undetachable entity from Puru’s Enterprises. Kudos to our partnership
           and collective vision!
         </p>
-        <p className={styles.link}>Visite Site: <Link href="https://haraaydesignstudio.com/" target="_black">@HaraayDesignStudio</Link></p>
+        <p className={styles.link}>
+          Visite Site:{" "}
+          <Link href="https://haraaydesignstudio.com/" target="_black">
+            @HaraayDesignStudio
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -1,14 +1,51 @@
+"use client";
+
 import React from "react";
 import styles from "./page.module.css";
 import Form from "@/compenents/Forms/Form";
 import { FaArrowDown } from "react-icons/fa";
+import logo from "../../../public/logo.png";
+import { useEffect } from "react";
+import Image from "next/image";
 
 function page() {
+  useEffect(() => {
+    const logospan = document.querySelectorAll(`.${styles.logo}`);
+    const intro = document.querySelector(`.${styles.intro}`);
+
+    setTimeout(() => {
+      logospan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add(styles.active);
+        }, (idx + 1) * 400);
+      });
+
+      setTimeout(() => {
+        logospan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.remove(styles.active);
+            span.classList.add(styles.fade);
+          }, (idx + 1) * 50);
+        });
+      }, 2000);
+
+      setTimeout(() => {
+        intro.style.top = "-100vh";
+      }, 2300);
+    }, 0);
+
+    return () => {};
+  }, []);
   const iconStyle = {
     strokeWidth: "1px", // Adjust the stroke width as needed
   };
   return (
     <div className={styles.main}>
+      <div className={styles.intro}>
+        <span className={styles.logo}>
+          <Image src={logo} alt="logo" />
+        </span>
+      </div>
       <div className={styles.container}>
         <div className={styles.namings}>
           <p className={styles.contact}>Contact Us</p>
