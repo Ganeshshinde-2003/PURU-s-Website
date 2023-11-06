0c629b7091dee85b664e4b92e785f60c7ba35d6d
Ganesh Shinde <115361691+Ganeshshinde-2003@users.noreply.github.com>
2023-11-06 12:38:36 +0000

    Added the google analytics

---
+++ src/app/GoogleAnalytics.js
@@ -0,0 +1,25 @@
+const GoogleAnalytics = ({ GA_TRACKING_ID }) => {
+    return (
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        ></script>
+      </head>
+    );
+  };
+  
+  export default GoogleAnalytics;
+  
